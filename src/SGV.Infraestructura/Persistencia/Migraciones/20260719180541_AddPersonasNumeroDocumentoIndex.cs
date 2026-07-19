cb361e72c03db7f302d0b55a5dbeadd12f943304
Sebastián Serrisuela <sebaserri@gmail.com>
2026-07-19 15:11:51 -0300

    fix(persistencia): add IX_Personas_NumeroDocumento index for sort performance
    Closes #176

---
+++ src/SGV.Infraestructura/Persistencia/Migraciones/20260719180541_AddPersonasNumeroDocumentoIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGV.Infraestructura.Persistencia.Migraciones
+{
+    /// <inheritdoc />
+    public partial class AddPersonasNumeroDocumentoIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Personas_NumeroDocumento",
+                table: "Personas",
+                column: "NumeroDocumento");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Personas_NumeroDocumento",
+                table: "Personas");
+        }
+    }
+}
